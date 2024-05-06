a757d2b777c2f4e014c8c79dc56e997c3428102a
lessw2020 <lessw@etrillium.com>
2024-05-06 13:43:22 -0700

    swizzle support

--- examples/matmul/this-sm90/pingpong_dupe.cu
+++ examples/matmul/this-sm90/pingpong_dupe.cu
@@ -49,5 +49,65 @@ inline CUtensorMapDataType to_CUtensorMapDataType() {
     }
 }
 
+enum class SmemSwizzleBits : uint8_t {
+    DISABLE = 0,
+    B32 = 1,
+    B64 = 2,
+    B128 = 3,
+};
+
+template <int B, int M, int S>
+HOST_DEVICE constexpr SmemSwizzleBits get_tma_swizzle_bits(Swizzle<B, M, S>) {
+    if constexpr (M==4) {
+        switch (B) {
+            default:
+            static assert(0 <=B && B <=3, "expected B = 0,1,2 or 3 when M==4. Unsupported layout swizzle.");
+            case 3:
+                return SmemSwizzleBits::B128;
+            case 2:
+                return SmemSwizzleBits::B64;
+            case 1:
+                return SmemSwizzleBits::B32;
+            case 0:
+                return SmemSwizzleBits::DISABLE;
+
+
+        }
+
+    }
+} else {
+    static_assert(M < 0, "unsupported layout swizzle.");
+}
+}
+
+
+inline CUtensorMapSwizzle to_CUtensorMapSwizzle(SmemSwizzleBits const& t) {
+  switch (t) {
+    default:
+      assert(false && "Unknown SmemSwizzleBits!");
+    case SmemSwizzleBits::DISABLE:
+      return CU_TENSOR_MAP_SWIZZLE_NONE;
+    case SmemSwizzleBits::B32:
+      return CU_TENSOR_MAP_SWIZZLE_32B;
+    case SmemSwizzleBits::B64:
+      return CU_TENSOR_MAP_SWIZZLE_64B;
+    case SmemSwizzleBits::B128:
+      return CU_TENSOR_MAP_SWIZZLE_128B;
+  }
+}
+
+
+}// minor dims move faster than major
+template <int BlockMajorSize, int BlockMinorSize, int B, int M, int S>
+TmaDescriptor make_tma_copy_desc(DType* gmem_ptr, int shape_major,
+                                int shape_minor,
+                                Swizzle<B,M,S> const& swizzle,
+                                uint32_t num_multicast) {
+void* gmem_address = (void*)gmem_ptr;
+uint64_t gmem_prob_shape[5] = {(uint64_t) shape_minor, (uint64_t)shape_major, 1,1,1};
+uint64)t gmem_prob_stride[5] = {sizeof(DType), sizeof(DType) * shape_minor, 0, 0,0};
+
+
+
 
 }
